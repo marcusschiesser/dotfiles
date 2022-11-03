f524e25b57f39822e2cb8b6baffd8c81ebb7ae76
Marcus Schiesser <mail@marcusschiesser.de>
2022-11-03 18:09:34 +0800

    ensure vim installation on linux

--- bootstrap.sh
+++ bootstrap.sh
@@ -21,11 +21,11 @@ if [ "$(uname)" == "Darwin" ]; then
   # Nushell MacOS hack
   ln -s $DOTFILEDIR/.config/nushell "$HOME/Library/Application Support"
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-  # Ensure that fish is installed under Linux
+  # Ensure that fish and vim are installed under Linux
   echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/3/Debian_11/ /' | sudo tee /etc/apt/sources.list.d/shells:fish:release:3.list
   curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:3/Debian_11/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_fish_release_3.gpg > /dev/null
   sudo apt-get update && DEBIAN_FRONTEND=noninteractive \
-      sudo -E apt-get -y install --no-install-recommends fish
+      sudo -E apt-get -y install --no-install-recommends fish vim
   # and oh-my-posh
   if ! [ -x "$(command -v oh-my-posh)" ]; then
     sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh && sudo chmod +x /usr/local/bin/oh-my-posh
