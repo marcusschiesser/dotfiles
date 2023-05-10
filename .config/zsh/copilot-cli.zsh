306783572cb61041fbc791291829658637ff7a74
Marcus Schiesser <mail@marcusschiesser.de>
2023-05-10 15:33:56 +0700

    install copilot cli if not yet installed

--- .config/zsh/copilot-cli.zsh
+++ .config/zsh/copilot-cli.zsh
@@ -1,1 +1,4 @@
-eval "$(github-copilot-cli alias -- "$0")"
+# Check if the github-copilot-cli command is available
+if command -v github-copilot-cli >/dev/null 2>&1; then
+    eval "$(github-copilot-cli alias -- "$0")"
+fi
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-DOTFILEDIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-dotfiles=$(ls -1 -A $DOTFILEDIR | grep -v -e .git -e bootstrap 2> /dev/null)
+DOTFILEDIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+dotfiles=$(ls -1 -A $DOTFILEDIR | grep -v -e .git -e bootstrap 2>/dev/null)
 
 if [[ $dotfiles ]]; then
   echo "Symlinking dotfiles..."
@@ -23,13 +23,19 @@ if [ "$(uname)" == "Darwin" ]; then
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
   # Ensure that fish and vim are installed under Linux
   echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/3/Debian_11/ /' | sudo tee /etc/apt/sources.list.d/shells:fish:release:3.list
-  curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:3/Debian_11/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_fish_release_3.gpg > /dev/null
+  curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:3/Debian_11/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_fish_release_3.gpg >/dev/null
   sudo apt-get update && DEBIAN_FRONTEND=noninteractive \
-      sudo -E apt-get -y install --no-install-recommends fish vim
+    sudo -E apt-get -y install --no-install-recommends fish vim
   # and oh-my-posh
   if ! [ -x "$(command -v oh-my-posh)" ]; then
     sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh && sudo chmod +x /usr/local/bin/oh-my-posh
   fi
 fi
 
+# Check if the package is already installed
+installed=$(npm list -g --depth=0 @githubnext/github-copilot-cli 2>/dev/null)
 
+# Install the package if it's not installed
+if [[ -z "$installed" ]]; then
+  npm install -g @githubnext/github-copilot-cli
+fi
