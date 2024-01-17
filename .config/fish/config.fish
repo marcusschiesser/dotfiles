9dbe6383676858066248084aab1dd994c727fdf8
Marcus Schiesser <mail@marcusschiesser.de>
2024-01-17 15:51:02 +0700

    add pnpm, node, orbstack

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,13 @@
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
 set -g fish_user_paths "$CHECKOUT/bin" $fish_user_paths
 set -g fish_user_paths "$HOME/bin" $fish_user_paths
-#set -x DOCKER_DEFAULT_PLATFORM linux/amd64
-#set -x DOCKER_HOST "ssh://ubuntu@docker.local"
+set -g fish_user_paths "/usr/local/opt/node@18/bin" $fish_user_paths
+set -x DOCKER_HOST "unix://$HOME/.orbstack/run/docker.sock"
 oh-my-posh init fish --config ~/.config/poshtheme.omp.json | source
+
+# pnpm
+set -gx PNPM_HOME "/Users/marcus/Library/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
